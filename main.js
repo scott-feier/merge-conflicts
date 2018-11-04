73b9baf6565491fb43de2893f7a92c68c7cb8695
scott-feier <scott.feier@gmail.com>
2018-11-04 15:15:05 -0500

    this is right

--- main.js
+++ main.js
@@ -1,8 +1,2 @@
 var num = 50;
-<<<<<<< HEAD
-var truth = "Hello Flatter Earth"
-=======
-var truth = "Hello Flat Earth"
-
-
->>>>>>> 904452ca909382fd305e5bfe922c4273b5d856f5
+var truth = "Hello Cats"
